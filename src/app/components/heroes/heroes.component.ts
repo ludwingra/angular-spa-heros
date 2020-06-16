02277da5d4686f8826cab6063bef0ea20ea6d140
ludwingra <ludwing.ra@gmail.com>
2020-06-15 20:53:41 -0500

    Componente heroes y servicio heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from "../../service/heroes.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styles: [
-  ]
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService) {
+
+  }
 
   ngOnInit(): void {
+
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes)
+
   }
 
 }
